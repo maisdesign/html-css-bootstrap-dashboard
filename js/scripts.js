05fbf422bf08800914d2022346103b00ba5b5cca
Maisdesign <downloadtaky@gmail.com>
2025-11-23 19:20:51 +0100

    Aggiunto effetto opacità al completamento di una task, ora passiamo alle F.A.Q.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const checks = document.querySelectorAll(".todo-check");
+
+  checks.forEach(check => {
+    check.addEventListener("change", () => {
+      const row = check.closest(".todo-item");
+      row.classList.toggle("completed", check.checked);
+    });
+  });
+});
