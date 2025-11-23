ce2f531bce6368a12acf90be3bc8a9f25e6ed3a3
Maisdesign <downloadtaky@gmail.com>
2025-11-23 21:21:52 +0100

    Aggiunta registrazione su LocalStorage dell'ultimo stato delle checkbox e rilettura delle stesse

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const checks = document.querySelectorAll(".todo-check");
+    const rawStates = localStorage.getItem("todoStates");
+    console.log("rawStates dal localStorage:", rawStates);
 
-  checks.forEach(check => {
-    check.addEventListener("change", () => {
-      const row = check.closest(".todo-item");
-      row.classList.toggle("completed", check.checked);
+    let todoStates;
+    if (rawStates === null) {
+        todoStates = {};
+    } else {
+        todoStates = JSON.parse(rawStates);
+    };
+    console.log("todoStates dal localStorage:", todoStates);
+    const checks = document.querySelectorAll(".todo-check");
+
+    checks.forEach(check => {
+        const id = check.id;
+        const savedState = todoStates[id];
+        if (savedState) {
+            check.checked = true;
+            const row = check.closest(".todo-item");
+            row.classList.add("completed");
+        }
     });
-  });
-});
 
-/**TODO implementare localstorage  */
+    checks.forEach(check => {
+        check.addEventListener("change", () => {
+            const row = check.closest(".todo-item");
+            row.classList.toggle("completed", check.checked);
+            const id = check.id;
+            todoStates[id] = check.checked;
+            localStorage.setItem("todoStates", JSON.stringify(todoStates));
+        });
+    });
+});
